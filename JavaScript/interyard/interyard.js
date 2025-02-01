c7e35f548ab4683752872978a60d81deca24b134
leeleeseop <iseobi138@gmail.com>
2025-02-02 00:48:51 +0900

    Update interyard.js

--- JavaScript/interyard/interyard.js
+++ JavaScript/interyard/interyard.js
@@ -1,14 +1,16 @@
 <script type="text/javascript">
   document.addEventListener("DOMContentLoaded", function () {
-      const images = document.querySelectorAll(".zoomable");
-      const modal = document.getElementById("imageModal");
-      const modalImg = document.getElementById("modalImg");
+    const images = document.querySelectorAll(".zoomable");
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImg");
+
     images.forEach(img => {
-      img.addEventListener("dbclick", function () {
-        modalImag.src = this.src;
+      img.addEventListener("dblclick", function () {
+        modalImg.src = this.src;
         modal.classList.add("active");
       });
     });
+
     modal.addEventListener("click", function () {
       modal.classList.remove("active");
     });
