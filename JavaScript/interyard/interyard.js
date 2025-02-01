961979beda8e20a3c0ae59317891ace21eaf7805
leeleeseop <iseobi138@gmail.com>
2025-02-01 21:41:40 +0900

    Update interyard.js

--- JavaScript/interyard/interyard.js
+++ JavaScript/interyard/interyard.js
@@ -1,10 +1,11 @@
 /**
- * const clickimg = document.getEleamentById("clickimg")
-   const modal = document.getEleamentById("modal")
-   const modalImg = document.getEleamentById("modalImg")
+ * 
+   const clickimg = document.getElementById("clickimg")
+   const modal = document.getElementById("modal")
+   const modalImg = document.getElementById("modalImg")
 
    // 이미지 클릭시 모달 열기
-   clickimg.addEvventListener("click", function() {
+   clickimg.addEventListener("click", function() {
      modalImg.src = this.src;
      modal.style.display = "flex";
    });
