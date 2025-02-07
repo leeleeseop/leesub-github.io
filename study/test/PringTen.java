1fd072e0c75459e6f2f08808664330f195239b9e
leeleeseop <iseobi138@gmail.com>
2025-02-07 15:30:44 +0900

    Create PringTen.java
    for 반복문을 사용하여 1부터 10까지 출력

---
+++ study/test/PringTen.java
@@ -0,0 +1,12 @@
+package test;
+
+public class PrintTen {
+
+	public static void main(String[] args) {
+		// for반복문을 사용하여 i 값을 1부터 10까지 증가
+		// int i = 1; i를 1로 초기화
+		// i <= 10; i가 10이하일 때 반복
+		// i++ 반복할때 마다 i값을 1씩 증가
+		for(int i = 1; i <= 10; i++) System.out.println(i);
+	}
+}
