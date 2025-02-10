73dc7e76933adbc05a70b795eb3fab720a2027cf
leeleeseop <iseobi138@gmail.com>
2025-02-10 10:56:57 +0900

    Create AddAndMultiClass.java

---
+++ study/Java/ch08Interface/AddAndMultiClass.java
@@ -0,0 +1,11 @@
+package ch08Interface;
+
+public class AddAndMultiClass implements AddCal {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return (a + b) * 100;
+	}
+
+}
