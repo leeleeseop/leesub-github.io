e3d48a9c5c5797d1091c6eb5561831393651edc3
leeleeseop <iseobi138@gmail.com>
2025-02-10 10:18:15 +0900

    Create NoticeVO.java

---
+++ study/Java/ch06class/NoticeVO.java
@@ -0,0 +1,38 @@
+package ch06class;
+
+// 번호, 제목, 작성일 - 저장
+public class NoticeVO {
+
+	// private 변수 선언.
+	private long no;
+	private String title;
+	private String writeDate;
+
+	// getter & setter
+	public long getNo() {
+		return no;
+	}
+	public void setNo(long no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+	
+	@Override // 상속받은 메서드와 동일한지 확인해 주는 어노테이션
+	public String toString() {
+		return "NoticeVO [ no=" + no
+				+ ", title=" + title
+				+ ", writeDate=" + writeDate
+				+ " ]";
+	}
+}
