b7db26f53568acb8080f509c3578397d47b116a3
leeleeseop <iseobi138@gmail.com>
2025-02-10 15:30:22 +0900

    Create DAO.java

---
+++ study/Java/web_jdbc/main.controller/main.dao/DAO.java
@@ -0,0 +1,12 @@
+package com.webjjang.main.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DAO {
+
+	public Connection con = null;
+	public PreparedStatement pstmt = null;
+	public ResultSet rs = null;
+}
