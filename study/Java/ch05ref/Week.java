0b88e9432e977c374a5f3db6323588a616b2a3cb
leeleeseop <iseobi138@gmail.com>
2025-02-10 10:02:43 +0900

    Create Week.java

---
+++ study/Java/ch05ref/Week.java
@@ -0,0 +1,17 @@
+package ch05ref;
+
+// class, interface, enum
+// class : 다 구현되어져 있는 코드
+// interface : 구현되지 않은 코드 - 같은 이름, 구현을 해서 사용
+// enum : 제한적인 데이터 사용 - 요일 : 7가지 데이터를 열거한다.
+public enum Week {
+
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+	
+}
