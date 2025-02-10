e8a9814d1f46a12f22e882eb1fac56f0ee2b5e26
leeleeseop <iseobi138@gmail.com>
2025-02-10 10:58:26 +0900

    Create Name1Class.java

---
+++ study/Java/ch08Interface/Name1Class.java
@@ -0,0 +1,11 @@
+package ch08Interface;
+
+public class Name1Class extends AbstractClass{
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("이름 : " + name);
+	}
+
+}
