49c6b92732acc28cbca3885d706d1b7f7858cf55
leeleeseop <iseobi138@gmail.com>
2025-02-10 15:35:34 +0900

    Create MemberViewService.java

---
+++ study/Java/web_jdbc/member.service/MemberViewService.java
@@ -0,0 +1,25 @@
+package com.webjjang.member.service;
+
+import java.util.List;
+
+import com.webjjang.main.service.Service;
+import com.webjjang.member.dao.MemberDAO;
+import com.webjjang.member.vo.MemberVO;
+
+public class MemberViewService implements Service {
+
+	@Override
+	public MemberVO service(Object obj) throws Exception {
+        
+        // MemberDAO 객체 생성
+   
+        // MemberDAO의 view 메서드 호출하여 결과를 반환
+        return new MemberDAO().view((String)obj);
+		//DB notice에서 리스트 쿼리 실행해서 데이터 가져오기 - 리턴
+		//DB 처리는 DAO에서 처리 NoticeDAO.list()
+		//NoticeController -> (Execute로그 출력) -> NoticeListService -> [NoticeDAO.list()]
+		//if(inc == 1) newdao.increase(no);
+		//return new MemberDAO().view((String)obj);
+	}
+
+}
