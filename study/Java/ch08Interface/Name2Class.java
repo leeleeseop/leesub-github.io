508d9d51134d0d5cb83db9f7eef5a0a5cc669da2
leeleeseop <iseobi138@gmail.com>
2025-02-10 10:58:42 +0900

    Create Name2Class.java

---
+++ study/Java/ch08Interface/Name2Class.java
@@ -0,0 +1,13 @@
+package ch08Interface;
+
+public class Name2Class extends AbstractClass {
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("***** 출력 *******");
+		System.out.println("** 이름 : " + name + " **");
+		System.out.println("*****************");
+	}
+
+}
