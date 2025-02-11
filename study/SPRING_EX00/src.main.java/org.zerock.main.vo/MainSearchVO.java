eadec30f36aa91c324012682d6fa99b510939e80
leeleeseop <iseobi138@gmail.com>
2025-02-11 11:49:50 +0900

    Create MainSearchVO.java

---
+++ study/SPRING_EX00/src.main.java/org.zerock.main.vo/MainSearchVO.java
@@ -0,0 +1,15 @@
+package org.zerock.main.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class MainSearchVO {
+
+	private Long no;
+	private String module;
+	private String title;
+	private Date writeDate;
+	
+}
