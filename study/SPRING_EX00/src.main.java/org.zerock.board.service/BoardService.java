ef0608458dc42f64838ac05eadb3940370acbd5b
leeleeseop <iseobi138@gmail.com>
2025-02-11 11:33:58 +0900

    Create BoardService.java

---
+++ study/SPRING_EX00/src.main.java/org.zerock.board.service/BoardService.java
@@ -0,0 +1,25 @@
+package org.zerock.board.service;
+
+import java.util.List;
+
+import org.zerock.board.vo.BoardVO;
+import com.webjjang.util.page.PageObject;
+
+public interface BoardService {
+
+	// 일반 게시판 리스트
+	public List<BoardVO> list(PageObject pageObject);
+	
+	// 일반 게시판 글보기
+	public BoardVO view(Long no, int inc);
+	
+	// 일반 게시판 글등록
+	public Integer write(BoardVO vo);
+	
+	// 일반 게시판 글수정
+	public Integer update(BoardVO vo);
+	
+	// 일반 게시판 글삭제
+	public Integer delete(BoardVO vo);
+	
+}
