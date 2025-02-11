f4ee00b93eb2d2e6b29bccf6971268995a84181e
leeleeseop <iseobi138@gmail.com>
2025-02-11 11:46:44 +0900

    Create GoodsSizeColorVO.java

---
+++ study/SPRING_EX00/src.main.java/org.zerock.goods.vo/GoodsSizeColorVO.java
@@ -0,0 +1,15 @@
+package org.zerock.goods.vo;
+
+import lombok.Data;
+
+@Data
+public class GoodsSizeColorVO {
+
+	private Long goods_sizeColor_no;
+	private Long goods_no;
+	private Long size_no;
+	private String size_name;
+	private Long color_no;
+	private String color_name;
+	
+}
