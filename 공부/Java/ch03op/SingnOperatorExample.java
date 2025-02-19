9c276941fc3c919ae42dccbfbb6aa98f4f6164a2
leeleeseop <iseobi138@gmail.com>
2025-02-19 11:29:04 +0900

    SingnOperatorExample.java
    부호를 변경

---
+++ 공부/Java/ch03op/SingnOperatorExample.java
@@ -0,0 +1,16 @@
+package ch03op;
+
+public class SingnOperatorExample {
+
+	public static void main(String[] args) {
+		
+		int x = -100;
+		x = -x;
+		System.out.println("x:" + x);
+		
+		byte b = 100;
+		int y = -b;
+		System.out.println("y:" + y);
+			
+	}// main end
+}// class end
