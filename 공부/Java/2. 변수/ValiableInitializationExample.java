22f342b8c3105e81bb85278ec20a6b6ee1a36ac5
leeleeseop <iseobi138@gmail.com>
2025-03-06 12:25:22 +0900

    변수 초기화 및 지역/전역 변수

--- 공부/Java/2. 변수/ValiableInitializationExample.java
+++ 공부/Java/2. 변수/ValiableInitializationExample.java
@@ -4,19 +4,24 @@ package ch02var;
 public class ValiableInitializationExample{
 	// static - 고정 : 자동으로 올라간다.
 	public static void main(String[] args){
+		
 		// 변수 선언 + 데이터 넣지 않음(초기화시키지 않음)
 		// 메서드 안에 선언된 변수 - 지역 변수
-		int value;
+		int value; //int 타입의 지역변수 value를 선언 
 		
 		// 프로그램이 진행되기 전에 값을 넣어주는 작업 = 초기화
-		value = 10;
+		value = 10; // value변수에 값10을 할당 이제 value변수는 정수 값 10으로 초기화
 		
 		// 변수 선언
+		// int 타입의 지역변수 result를 선언하고 value + 10을 더하여 연산결과 20으로 초기화
 		int result = value + 10; // - 지역변수는 초기화 시키지 않으면 오류남.
+		
 		// 지역 변수는 메서드 안에 선언된 변수 - 초기화 시키지 않으면 값이 없음
 		// 전역 변수는 메서드 안에 선언되지 않은 변수
 		//  - 초기화 시키지 않으면 기본 값을 갖는다. 숫자인 경우 0
 		// int result = ValiableInitializationExample.value + 10;
+
+		// result변수의 값(20)을 출력
 		System.out.println(result);
 	}
 	// 메서드 선언
