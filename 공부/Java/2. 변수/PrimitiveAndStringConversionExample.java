aceda80cab208231361aed2d21a27eecea34b8d9
leeleeseop <iseobi138@gmail.com>
2025-03-06 10:53:34 +0900

    문자열-기본형 변환 및 int범위 출력
    사용자 입력으로 받은 숫자 문자열을 정수로 변환하거나  계산 결과인 정수를 문자열로 변환할때

--- 공부/Java/2. 변수/PrimitiveAndStringConversionExample.java
+++ 공부/Java/2. 변수/PrimitiveAndStringConversionExample.java
@@ -14,14 +14,19 @@ public class PrimitiveAndStringConversionExample {
 		//    char : Character, boolean : Boolean
 		int value1 = Integer.parseInt("10");
 		System.out.println("value1 : " + value1);
-		
+
+		// String.valueOf(value1): 다양한 타입의 값을 String 타입으로 변환 기
 		String str1 = String.valueOf(value1);
 		System.out.println(str1);
+
+		// str1이 String타입인지 확
 		System.out.println(str1 instanceof String);
-		
+
+		// value1에 빈 문자열을 더하여 생성한 결과를 String 타입인지 확인
+		// (value1 + "")는 int와 문자열의 결합 연산 결과는 문자열
 		System.out.println((value1 + "") instanceof String);
 		
-		// int의 최대 사용 값과 최소 사용값을 출력해 보세요.
+		// int의 최대 사용 값과 최소 사용값을 출력
 		System.out.println(Integer.MAX_VALUE);
 		System.out.println(Integer.MIN_VALUE);
 	}
