4c5cd45a799f1f01b21df2c3642992d6752903fd
leeleeseop <iseobi138@gmail.com>
2025-03-08 17:55:50 +0900

    논리 연산자를 사용하여 null 안전성 검사
    객체가 null인 경우에는 메서드 호출을 피하여 NullPointerException을 예방

--- 공부/Java/3. 연산자/LogicalOperatorExample.java
+++ 공부/Java/3. 연산자/LogicalOperatorExample.java
@@ -3,11 +3,14 @@ package ch03op;
 public class LogicalOperatorExample {
 
 	public static void main(String[] args) {
-		// and - & / &&, or - | / ||, xor - ^ : true 홀수개, not - !
-		String name = null; // 참조형 변수인데 주소가 없음
-		// name.equals("lee") -> name이 null이면 찾을 곳이 없어서 오류
-		// System.out.println(name != null & name.equals("lee"));
+		
+		// 참조형 변수 name을 null로 초기화
+		String name = null;
+
+		// &&(논리 AND) 연산자를 사용하여 첫 번째 조건(name != null)이 false일 경우 두 번째 조건은 평가되지 않음
 		System.out.println(name != null && name.equals("lee"));
+
+		// name이 null인지 확인하여 결과를 출
 		System.out.println(name != null);
 	}
 
