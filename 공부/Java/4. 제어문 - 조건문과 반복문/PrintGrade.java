138b247b1a88d7cc43df7bb34003c0aa251bddd2
leeleeseop <iseobi138@gmail.com>
2025-03-10 14:55:04 +0900

    점수를 입력받아 등급 계산 및 오류 처리

--- 공부/Java/4. 제어문 - 조건문과 반복문/PrintGrade.java
+++ 공부/Java/4. 제어문 - 조건문과 반복문/PrintGrade.java
@@ -13,8 +13,8 @@ public class PrintGrade {
 	public static void main(String[] args) {
 		
 		// console에서 입력할 때 사용되는 객체 변수 선언 초기값(키보드)
+		// 사용자에게 점수 입력요청 출력
 		Scanner scanner = new Scanner(System.in);
-		
 		System.out.print("점수 입력 -> ");
 		// 점수 입력
 		int score = Integer.parseInt(scanner.nextLine());
@@ -29,15 +29,15 @@ public class PrintGrade {
 		
 		// 2. 등급 계산
 		char grade;
-		// 1번 if
-		if(score > 100) grade = 'O';
+		
+		// if-else문을 사용하여 점수에 따른 등급을 계산
+		if(score > 100) grade = 'O'; // 점수가 100보다 크면 오류처리
 		else if(score >= 90) grade = 'A';
 		else if(score >= 80) grade = 'B';
 		else if(score >= 70) grade = 'C';
 		else if(score >= 60) grade = 'D';
 		else if(score >= 0) grade = 'F';
-		else grade = 'U';
-		// 1번 if의 끝
+		else grade = 'U'; // 점수가 0미만이면 잘못된 처리로 U처리
 		
 		// 등급 출력 - O, U 처리한다.
 		if(grade == 'O') System.out.println("점수 오류 : 100 초과");
@@ -56,9 +56,7 @@ public class PrintGrade {
 			break;
 		}
 		
-		// }
 		// 테스트 반복 끝
-		
 		scanner.close(); // scanner 자원 닫기 = 자원 회수
 		
 	} // main() 끝
