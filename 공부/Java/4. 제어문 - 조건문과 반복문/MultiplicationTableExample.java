382ebfc79bb5367a7070a9f060e1539bf367e952
leeleeseop <iseobi138@gmail.com>
2025-03-10 11:41:28 +0900

    구구단 출력
    중첩 for문을 사용하여 복잡한 반복 구조를 단순환

--- 공부/Java/4. 제어문 - 조건문과 반복문/MultiplicationTableExample.java
+++ 공부/Java/4. 제어문 - 조건문과 반복문/MultiplicationTableExample.java
@@ -12,12 +12,21 @@ public class MultiplicationTableExample {
 
 	public static void main(String[] args) {
 		
-		// 단에 해당되는 처리 : 2 ~ 9
+		// 바깥쪽 for문: 단(구구단의 '단' 부분) - 2부터 9까지 반복
+		// int i=2 : i는 현재 단 수, 2단부터 시작 초기값 2로 설정
+		// i<=9 : i가 9보다 작거나 같으면 계속 반복 구구단은 9단까지 출력
+		// i++ : 1씩 증가
 		for(int i = 2; i <= 9; i ++) {
-			// 곱해지는 수 : 1 ~ 9
+			
+			// 안쪽 for문: 곱해지는 수 1부터 9까지 반복
+			// int j=1 : j는 현재 곱해지는 수, 1부터 9까지 곱하므로 초기값 1로 설정
+			// j<=9 : j가 9보다 작거나 같으면 계속 반복 
+			// j++ : j값을 1씩 증가
 			for(int j = 1; j <= 9; j++) {
-				// i = 2, j = 1 ... i = 2, j = 9 / i = 3, j= 1 ... i = 3, j = 9
+				// 구구단 각 항목을 출력
 				System.out.print(i + " * " + j + " = " + (i * j));
+
+				// 3개씩 출력 후 줄바꿈 처리 한 줄에 3개의 구구단 항목 출력
 				if(j % 3 == 0) {
 					System.out.println();
 				} else {
