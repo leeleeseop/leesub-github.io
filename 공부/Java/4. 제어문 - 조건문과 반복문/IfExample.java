47e482cbf50d91bd67da4d4190266e28f36349f4
leeleeseop <iseobi138@gmail.com>
2025-03-10 10:11:51 +0900

    if문을 통한 점수 평가 출력
    조건에 따라 서로 다른 결과를 출력

--- 공부/Java/4. 제어문 - 조건문과 반복문/IfExample.java
+++ 공부/Java/4. 제어문 - 조건문과 반복문/IfExample.java
@@ -3,13 +3,19 @@ package ch04;
 public class IfExample {
 
 	public static void main(String[] args) {
+
+		// score변수를 93으로 초기화
 		int score = 93;
-		
+
+		// 첫 번째 if문 score가 90이상이면 실행
+		// 93은 90이상이므로 출력
 		if(score >= 90) {
 			System.out.println("점수가 90보다 큽니다.");
 			System.out.println("등급은 A입니다");
 		}
-		
+
+		// 두 번째 if 문: score가 90미만이면 실행
+		// score가 93이므로 출력되지가 않음
 		if(score < 90) {
 			System.out.println("점수가 90보다 작습니다.");
 			System.out.println("등급은 B입니다");
