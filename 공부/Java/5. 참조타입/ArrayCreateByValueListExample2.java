6bf6fb4ca535babfb198cdef5e399af1ff223243
leeleeseop <iseobi138@gmail.com>
2025-03-10 16:31:52 +0900

    배열 초기화, 합계 계산 및 배열 출력

--- 공부/Java/5. 참조타입/ArrayCreateByValueListExample2.java
+++ 공부/Java/5. 참조타입/ArrayCreateByValueListExample2.java
@@ -7,7 +7,7 @@ public class ArrayCreateByValueListExample2 {
 		//배열 변수 선언
 		int[] scores;
 		
-		//배열 변수에 배열 대입
+		//배열 변수에 배열 대입: 배열 scoreas에 83, 90, 87값을 저장
 		scores = new int[] { 83, 90, 87 };
 		
 		// 배열 항목의 총합을 구하고 출력
