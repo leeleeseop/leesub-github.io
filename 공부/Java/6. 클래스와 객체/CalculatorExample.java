48053a109d758a728d22d3ac98c210327a89c926
leeleeseop <iseobi138@gmail.com>
2025-03-12 08:51:19 +0900

    static import 및 인스턴스 메서드 활용

--- 공부/Java/6. 클래스와 객체/CalculatorExample.java
+++ 공부/Java/6. 클래스와 객체/CalculatorExample.java
@@ -6,11 +6,12 @@ import static ch06class.CalculatorClass.multi;
 public class CalculatorExample {
 
 	public static void main(String[] args) {
+		
 		// 계산기 사용.
+		// 두 정수 var1과 var2를 선언 10과 3선언
 		int var1 = 10, var2 = 3;
 		
 		// static 메서드는 new하지 않고 사용 - 자주 사용되는 메서드에 static 선언
-		// CalculatorClass.multi(var1, var2);
 		// static import가 되어 있어야 사용가능
 		multi(var1, var2);
 		
@@ -18,7 +19,5 @@ public class CalculatorExample {
 		CalculatorClass cal = new CalculatorClass();
 		// 더하기 처리하고 출력
 		cal.add(var1, var2);
-		
-		
 	}
 }
