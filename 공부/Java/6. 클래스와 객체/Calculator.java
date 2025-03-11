4a65f4b0917995ce18743f1db6f8762117a1efa4
leeleeseop <iseobi138@gmail.com>
2025-03-11 15:56:34 +0900

    기본 산술연산

--- 공부/Java/6. 클래스와 객체/Calculator.java
+++ 공부/Java/6. 클래스와 객체/Calculator.java
@@ -9,19 +9,24 @@ public class Calculator {
 		// 값을 세팅
 		int var1 = 10;
 		int var2 = 3;
-		
+
+		// var1과 var2를 더한 결과를 result에 저장하고 출력
 		int result = var1 + var2;
 		System.out.println(var1 + " + " + var2 + " = " + result);
-		
+
+		// var1과 var2를 뺀 결과를 result에 저장하고 출력
 		result = var1 - var2;
 		System.out.println(var1 + " - " + var2 + " = " + result);
-		
+
+		// var1과 var2를 곱한 결과를 result에 저장하고 출력
 		result = var1 * var2;
 		System.out.println(var1 + " * " + var2 + " = " + result);
-		
+
+		// var1과 var2를 나눈 결과를 result에 저장하고 출력
 		result = var1 / var2;
-		System.out.println(var1 + " * " + var2 + " = " + result);
-		
+		System.out.println(var1 + " / " + var2 + " = " + result);
+
+		// var1과 var2를 나눈 나머지 결과를 result에 저장하고 출력
 		result = var1 % var2;
 		System.out.println(var1 + " % " + var2 + " = " + result);
 		
