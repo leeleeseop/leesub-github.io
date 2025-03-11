b69d51e10dd256dd75c41960e04c4aec3c4286ea
leeleeseop <iseobi138@gmail.com>
2025-03-12 08:46:36 +0900

    두 정수에 대한 기본 산술연산

--- 공부/Java/6. 클래스와 객체/CalculatorClass.java
+++ 공부/Java/6. 클래스와 객체/CalculatorClass.java
@@ -6,27 +6,31 @@ public class CalculatorClass {
 	// 메서드를 작성해서 호출해서 사용해 보자. 선언 부분
 	// 더해서 출력하는 메서드
 	public void add(int var1, int var2) {
-			int result = var1 + var2;
-			resultPrint(var1, var2, result, "+");
-			return;
+		int result = var1 + var2;
+		resultPrint(var1, var2, result, "+");
+		return;
 	 }
+	
 	 // 빼서 출력하는 메서드 - minus()
 	public void minus(int var1, int var2) {
 			int result = var1 - var2;
 			// System.out.println(var1 + " - " + var2 + " = " + result);
 			resultPrint(var1, var2, result, "-");
 	 }
+	
 	 // 곱하는 메서드 - multi()
 	// static인 경우 자동으로 미리 올라간다. resultPrint가 메인메모리에 올라와사 사용가능
 	public static void multi(int var1, int var2) {
 		 int result = var1 * var2;
 		 staticResultPrint(var1, var2, result, "*");
 	 }
+	
 	 // 나누는 메서드 - divide()
 	public void divide(int var1, int var2) {
 		 int result = var1 / var2;
 		 resultPrint(var1, var2, result, "/");
 	 }
+	
 	 // 나머지 메서드 - remain()
 	public void remain(int var1, int var2) {
 		 int result = var1 % var2;
