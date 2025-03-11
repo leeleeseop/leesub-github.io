5e39aed60d2fd4e3db00d03b49c8e32a7919c23a
leeleeseop <iseobi138@gmail.com>
2025-03-11 11:22:42 +0900

    배열의 데이터를 오름차순 정렬할때 기본적인 선택 정렬

--- 공부/Java/5. 참조타입/SortArray.java
+++ 공부/Java/5. 참조타입/SortArray.java
@@ -8,22 +8,25 @@ public class SortArray {
 	public static void main(String[] args) {
 		// 원본 데이터를 배열로 정의
 		int[] nums = {5, 7, 2, 1, 6};
+		
 		// 원본 데이터를 출력
 		System.out.println(Arrays.toString(nums));
+		
 		// 중첩 for select  소트 시작
 		// 맨 앞에 부터 차례로 작은 데이터로 정렬 시키기위해 선택한다. -> 마지막은 자동 정렬
 		for(int i = 0; i < nums.length - 1; i++) {
 			// 선택한 다음 데이터부터 마지막 데이터까지
 			for(int j = i + 1; j < nums.length; j++) {
 				// 선택된 데이터가 비교 데이터 보다 더 크면 바꾼다.
+				// nums[i] 가 nums[j]보다 크면 두 값을 교환
 				if(nums[i] > nums[j]) {
 					int temp = nums[i];
 					nums[i] = nums[j];
 					nums[j] = temp;
-				}
+				} // end of for if
 			} // end of for j
-			// System.out.println(Arrays.toString(nums));
 		} // end of for i
+		
 		// 중첩 for select  소트 끝
 		// 정렬된 데이터 출력
 		System.out.println(Arrays.toString(nums));
