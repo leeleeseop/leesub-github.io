900fb9f6927c828085626d6adde75fa803a972b2
leeleeseop <iseobi138@gmail.com>
2025-03-11 15:25:25 +0900

    일반게시판의 데이터를 저장하고 처리

--- 공부/Java/6. 클래스와 객체/Board.java
+++ 공부/Java/6. 클래스와 객체/Board.java
@@ -22,10 +22,13 @@ public class Board {
 	public void setNo(long no) {
 		this.no = no;
 	}
+	
 	public String getTitle() {return title;}
 	public void setTitle(String title) {this.title = title;}
+	
 	public String getContent() {return content;}
 	public void setContent(String content) {this.content = content;}
+	
 	public String getWriter() {return writer;}
 	public void setWriter(String writer) {this.writer = writer;}
 	
