14920dc214c30867f6f98c0c8bde06be9155a17b
leeleeseop <iseobi138@gmail.com>
2025-03-12 14:20:39 +0900

    다른 클래스에서 Super 클래스의 변수에 접근할 때

--- 공부/Java/7. 상속/ch07basic/SuperExample.java
+++ 공부/Java/7. 상속/ch07basic/SuperExample.java
@@ -3,11 +3,18 @@ package ch07basic;
 public class SuperExample {
 
 	public static void main(String[] args) {
+
+		// 객체 생성
 		Super super1 = new Super();
+		
 		System.out.println(super1.publicVar);
+		
 		System.out.println(super1.protectedVar);
+		
 		System.out.println(super1.defaultVar);
-//		System.out.println(super1.privateVar); // private 변수는 다른 클래스에서 사용 불가.
+		
+		// private 변수는 다른 클래스에서 사용 불가.
+		// System.out.println(super1.privateVar);
 	}
 	
 }
