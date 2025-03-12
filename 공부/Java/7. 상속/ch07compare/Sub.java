eec568023e10e0df4e5fabd926b73fc954807a64
leeleeseop <iseobi138@gmail.com>
2025-03-12 15:52:26 +0900

    Sub 클래스의 상속 및 확장 기능 구현

--- 공부/Java/7. 상속/ch07compare/Sub.java
+++ 공부/Java/7. 상속/ch07compare/Sub.java
@@ -6,8 +6,11 @@ package ch07compare;
 // 변수 name과 add()를 가지고 있는 클래스
 public class Sub extends Super {
 
+	// Sub 클래스 고유의 멤버 변수
 	String name = "이이섭";
-	
+
+	// add() 메서드
+	// Super 클래스에서 상속받은 a를 1 증가
 	public void add() {
 		a ++;
 	}
