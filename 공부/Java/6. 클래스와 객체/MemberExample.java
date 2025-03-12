17f9fdf9a46105219f6faad6df0c6492754a9b0b
leeleeseop <iseobi138@gmail.com>
2025-03-12 10:31:45 +0900

    Member 객체 생성 및 데이터 초기화

--- 공부/Java/6. 클래스와 객체/MemberExample.java
+++ 공부/Java/6. 클래스와 객체/MemberExample.java
@@ -5,17 +5,18 @@ public class MemberExample {
 	public static void main(String[] args) {
 		
 		// Member를 불러와서 사용해 보자.
+		// 기본 생성자는 내부에서 기본 값을 설정
 		Member m1 = new Member(); // static을 자동으로 클래서 이름이 불려지면 올라간다.
 		System.out.println(m1.name);
 		Member m2 = new Member(); // static을 자동으로 클래서 이름이 불려지면 올라간다.
 		System.out.println(m2.name);
 		System.out.println(Member.className);
-		// System.out.println(m1.className);
 		
 		// 생성자를 사용 데이터 초기화
-		Member m3 = new Member("admin", "이영환", 40);
+		Member m3 = new Member("admin", "이이섭", 26);
 		System.out.println(m3.name);
-		m3.name = "손흥민";
+		// 이섭이로 변경
+		m3.name = "이섭이";
 		System.out.println(m3.name);
 	} // end of main()
 } // end of class
