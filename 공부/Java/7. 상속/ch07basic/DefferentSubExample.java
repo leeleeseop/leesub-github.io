77caabc6668dee3d6cd3b8e80dca99372c4333c6
leeleeseop <iseobi138@gmail.com>
2025-03-12 11:44:41 +0900

    다른 패키지에 있는 클래스를 가져와서 그 클래스의 접근할때 사용

--- 공부/Java/7. 상속/ch07basic/DefferentSubExample.java
+++ 공부/Java/7. 상속/ch07basic/DefferentSubExample.java
@@ -4,11 +4,20 @@ import ch07defferent.Sub;
 public class DefferentSubExample {
 
 	public static void main(String[] args) {
+
+		// sub 클래스의 객체 생성
 		Sub sub = new Sub();
+
+		// public 변수는 모든 곳에서 접근이 가능
 		System.out.println(sub.publicVar);
+
+		// protected 변수는 동일 패키지 또는 서브클래스에서 접근할 수 있음
+		// 다른 패키지에 있는 클래스라도 sub가 상속관계라면 접근이 가능하지만
+		// 상속 관계가 없으면 접근이 불가능
 		System.out.println(sub.protectedVar);
-//		System.out.println(super1.defaultVar);// default 변수는 다른 패키지에서 사용 불가.
-//		System.out.println(super1.privateVar); // private 변수는 다른 클래스에서 사용 불가.
+
+		// System.out.println(super1.defaultVar);// default 변수는 다른 패키지에서 사용 불가.
+		// System.out.println(super1.privateVar); // private 변수는 다른 클래스에서 사용 불가.
 	}
 	
 }
