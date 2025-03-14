4191d97f645b8d9387014ce3cb98470206bc5a3c
leeleeseop <iseobi138@gmail.com>
2025-03-14 09:32:11 +0900

    FinalSub 클래스에서 final 클래스 상속 제한 규칙

--- 공부/Java/7. 상속/ch07final/FinalSub.java
+++ 공부/Java/7. 상속/ch07final/FinalSub.java
@@ -1,6 +1,8 @@
 package ch07final;
 
 public class FinalSub {
-//public class FinalSub extends FinalSuper { // final class를 상속 받을 수 없다.
 
-}
+  // final class를 상속 받을 수 없다.
+  //public class FinalSub extends FinalSuper { 
+
+  }
