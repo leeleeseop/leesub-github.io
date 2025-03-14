228dd735f12a83c4ddbddb2d66175e357285f985
leeleeseop <iseobi138@gmail.com>
2025-03-14 10:45:59 +0900

    InheExmple 클레스의 상속 객체 생성 및 메서드 호출

--- 공부/Java/7. 상속/ch07inhe/InheExample.java
+++ 공부/Java/7. 상속/ch07inhe/InheExample.java
@@ -3,9 +3,16 @@ package ch07inhe;
 public class InheExample {
 
 	public static void main(String[] args) {
+		
 		// Super 클래스를 상속받은 Sub 클래스 생성
+		// Sub 클래스의 객체를 생성하고 상속받은 변수 x를 수정한 후
+		// Sub 클래스에 Override된 print() 메서드를 호출합니다.
 		Sub sub1 = new Sub();
+
+		// Super 클래스에서 상속받은 변수 x의 값을 100으로 설정
 		sub1.x = 100;
+
+		// print() 메서드를 호출하여 객체의 상태를 출력
 		sub1.print();
 	}
 	
