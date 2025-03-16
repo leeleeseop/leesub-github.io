e8e64df56d98a92aae0ee53f28774517ad997c9c
leeleeseop <iseobi138@gmail.com>
2025-03-17 07:48:59 +0900

    AddAndMultiClass 클래스의 add() 메소드 구
    인터페이스의 정의된 메서드 구현을 통해 두 정수의 연산 결과에 추가적인 처리

--- 공부/Java/8. 추상 클래스와 인터페이스/AddAndMultiClass.java
+++ 공부/Java/8. 추상 클래스와 인터페이스/AddAndMultiClass.java
@@ -4,8 +4,8 @@ public class AddAndMultiClass implements AddCal {
 
 	@Override
 	public int add(int a, int b) {
-		// TODO Auto-generated method stub
+
+		// 두 정수를 더한 후 결과 100을 곱해서 반환
 		return (a + b) * 100;
 	}
-
 }
