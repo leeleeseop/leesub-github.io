0338ad28838fcbaca1b4676a3a37325ea2329d17
leeleeseop <iseobi138@gmail.com>
2025-03-17 08:04:51 +0900

    Name1Class 클래스 AbstractClass 추상 메서드

--- 공부/Java/8. 추상 클래스와 인터페이스/Name1Class.java
+++ 공부/Java/8. 추상 클래스와 인터페이스/Name1Class.java
@@ -1,10 +1,12 @@
 package ch08Interface;
 
+// Name1Class는 AbstractClass를 상속받아 추상 메서드 print()를 구현
 public class Name1Class extends AbstractClass{
 
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
+		
+		// 추상 메서드를 print()를 구현하여 name의 값을 "이름 : " 와 함께 출력
 		System.out.println("이름 : " + name);
 	}
 
