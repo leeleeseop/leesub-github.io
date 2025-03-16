f7d83a2a8dcef2ca9875e4a3ed048be121d73f6b
leeleeseop <iseobi138@gmail.com>
2025-03-17 08:14:14 +0900

    NameExample 클래스의 AbstractClass 활용

--- 공부/Java/8. 추상 클래스와 인터페이스/NameExample.java
+++ 공부/Java/8. 추상 클래스와 인터페이스/NameExample.java
@@ -3,10 +3,14 @@ package ch08Interface;
 public class NameExample {
 
 	public static void main(String[] args) {
+
+		// AbstractClass 타입의 변수 name1에 NmaeClass 객체를 할당
+		// AbstractClass의 추상 메서드 print()를 구현하여 이름을 출력
 		AbstractClass name1 = new Name1Class();
 		name1.setAndPrint("홍길동");
+		
 		AbstractClass name2 = new Name2Class();
-		name2.setAndPrint("손흥민");
+		name2.setAndPrint("길동홍");
 	}
 	
 }
