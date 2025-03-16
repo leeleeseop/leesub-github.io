e53f061d9da80aaf63d5ff297cabb2eef34e0951
leeleeseop <iseobi138@gmail.com>
2025-03-17 08:22:43 +0900

    EceptionExample 클래스의 예외처리 및 메서드 호출
    프로그램 실행 중 예외 상황을 처리할때 직접 try-catch 구문 또는 메서드 호출

--- 공부/Java/9. 예외처리/EceptionExample.java
+++ 공부/Java/9. 예외처리/EceptionExample.java
@@ -3,8 +3,8 @@ package ch09exception;
 public class EceptionExample {
 
 	public static void main(String[] args) {
-		//1. 데이터 세팅 2. 연산 /0 3.출력
 		
+		//1. 데이터 세팅 2. 연산 /0 3.출력
 		System.out.println("프로그램 시작");
 		int a = 10;
 		int result = 0;
@@ -18,23 +18,29 @@ public class EceptionExample {
 //		} finally {
 //			System.out.println("반드시 실행되는 부분");
 //		}
+
 		
 		// 메서드 호출로 예외처리
 		try {
 			result = divide(a);
 		} catch (Exception e) {
 
+			// 발생한 예외처리 StackTrace를 출력
 			e.printStackTrace();
 		}
-		
+
+		// 결과 출력 (예외 발생시 result는 0으로 남게 됨)
 		System.out.println(result);
 		
 		System.out.println("프로그램 끝");
 	}// end of main()
-	
+
+	// divide 메서드 입력된 정수를 0으로 나누어 예외를 발생시킴
+	// 이 메서드는 예외 발생 가능성이 있으므로 throws Exception을 선언
 	public static int divide(int a) throws Exception {
 		int result = 0;
-		
+
+		// a를 0으로 나누면 ArithmeticException이 발생
 		result = a / 0;
 		System.out.println("연산완료");
 		
