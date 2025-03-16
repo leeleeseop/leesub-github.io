d9f745502587e27b94e2e3b7f6b5ca0893d06ade
leeleeseop <iseobi138@gmail.com>
2025-03-17 07:56:51 +0900

    AddClass 클래스의 add() 메서드 구현

--- 공부/Java/8. 추상 클래스와 인터페이스/AddClass.java
+++ 공부/Java/8. 추상 클래스와 인터페이스/AddClass.java
@@ -1,10 +1,13 @@
 package ch08Interface;
 
+// AddClass는 AddCal 인터페이스를 구현하는 클래스
+// 아 클래스는 두 정수를 더한 결과를 반환하는 add() 메서드
 public class AddClass implements AddCal {
 
 	@Override
 	public int add(int a, int b) {
-		// TODO Auto-generated method stub
+		
+		// 두 정수를 더하여 결과를 반환 
 		return a + b;
 	}
 
