e18e3572b93151c7c2270844d0a5f67c1bd43815
leeleeseop <iseobi138@gmail.com>
2025-03-17 09:10:25 +0900

    BoardDAO의 게시판 CRUD 기능 및 SQL 쿼리 실행
    일반게시판 데이터 접근 DAO에서 데이터베이스와의 연결을 통해 게시글 목록 조회, 글보기, 글수정, 글삭제 등의 CRUD 작업을 수행

--- 공부/Java/web_jdbc/board.dao/BoardDAO.java
+++ 공부/Java/web_jdbc/board.dao/BoardDAO.java
@@ -15,12 +15,9 @@ public class BoardDAO extends DAO {
 	//1.리스트 처리
 	//BoardController -> (Execute로그 출력) -> BoardListService -> [BoardDAO.list()]
 	public List<BoardVO> list()  throws Exception{
-		
 		//결과를 저장할 수 있는 변수 선언
 		List<BoardVO> list = null;
-		
 		try {
-			
 			//1. 드라이버 확인
 			//2. 연결
 			con = DB.getConnection();
@@ -45,8 +42,8 @@ public class BoardDAO extends DAO {
 					
 					//vo - > list에 담는다
 					list.add(vo);
-				}
-			}
+				} // end of while 
+			} // end of if
 		}catch(Exception e ) {
 			e.printStackTrace();
 			throw e;
@@ -60,54 +57,48 @@ public class BoardDAO extends DAO {
 	}//end of public List<BoardVO> list()
 	
 	
-	
-	
-	
 	//2-1 글보기 조회수 1증가 처리
 	//1.리스트 처리
-		//BoardController -> (Execute로그 출력) -> BoardViewService -> [BoardDAO.list()]
-		public int increase(Long no)  throws Exception{
+	//BoardController -> (Execute로그 출력) -> BoardViewService -> [BoardDAO.list()]
+	public int increase(Long no)  throws Exception{
+		
+		//결과를 저장할 수 있는 변수 선언.
+		int result = 0;
+		try {
+			//1. 드라이버 확인
 			
-			//결과를 저장할 수 있는 변수 선언.
-			int result = 0;
-			try {
-				//1. 드라이버 확인
-				
-				//2. 연결
-				con = DB.getConnection();
-				
-				//3. sql - 아래 list
-				
-				//4. 실행객체 & 데이터 세팅
-				pstmt = con.prepareStatement(INCREASE);
-				pstmt.setLong(1, no);
-				
-				//5. 실행 실행 update : sxecuteUpdate() - int 결과가 나옴
-				result = pstmt.executeUpdate();
-				
-				//6. 표시 또는 담기
-				if(result == 0) {  //글번호가 존재하지 않는다 -> 예외로 처리한다
-					throw new Exception("예외 발생 : 글번호가 존재하지 않습니다. 글번호를 확인해 주세요");
-				}//end of if
+			//2. 연결
+			con = DB.getConnection();
 			
-			}catch(Exception e ) {
-				e.printStackTrace();
-				//특별한 예외는 그냥 전달한다
-				if(e.getMessage().indexOf("예외발생") >= 0) throw e;
-				//그외 처리 중 나타나는 오류에 대해서 시용자가 볼수 있는 예외로 만들어 전달한다.
-				else throw new Exception("예외 발생: 게시판 글보기 조회수 DB처리 중 예외가 발생했습니다");
-			}finally {	
-				//7. 닫기
-				DB.close(con, pstmt);
-			}
+			//3. sql - 아래 list
 			
-			//결과 데이터를 리턴해 준다.
-			return result;
-		}// end of public int increase
+			//4. 실행객체 & 데이터 세팅
+			pstmt = con.prepareStatement(INCREASE);
+			pstmt.setLong(1, no);
+			
+			//5. 실행 실행 update : sxecuteUpdate() - int 결과가 나옴
+			result = pstmt.executeUpdate();
+			
+			//6. 표시 또는 담기
+			if(result == 0) {  //글번호가 존재하지 않는다 -> 예외로 처리한다
+				throw new Exception("예외 발생 : 글번호가 존재하지 않습니다. 글번호를 확인해 주세요");
+			}//end of if
 		
+		}catch(Exception e ) {
+			e.printStackTrace();
+			//특별한 예외는 그냥 전달한다
+			if(e.getMessage().indexOf("예외발생") >= 0) throw e;
+			//그외 처리 중 나타나는 오류에 대해서 시용자가 볼수 있는 예외로 만들어 전달한다.
+			else throw new Exception("예외 발생: 게시판 글보기 조회수 DB처리 중 예외가 발생했습니다");
+		}finally {	
+			//7. 닫기
+			DB.close(con, pstmt);
+		}
 		
-		
-		
+		//결과 데이터를 리턴해 준다.
+		return result;
+	}// end of public int increase
+	
 	//2-2 글보기에 대한 처리
 	//1.리스트 처리
 	//BoardController -> (Execute로그 출력) -> BoardListService -> [BoardDAO.view()]
@@ -148,54 +139,53 @@ public class BoardDAO extends DAO {
 		}//end of public view
 		
 		
-		
 	//3.글등록
 	//1.리스트 처리
 	//BoardController -> (Execute로그 출력) -> BoardViewService -> [BoardDAO.list()]
 	public BoardVO write(Object vo)  throws Exception{
 				
-				//결과를 저장할 수 있는 변수 선언.
-				
-				int result = 0;
-				try {
-					//1. 드라이버 확인
-					
-					//2. 연결
-					con = DB.getConnection();
-					
-					//3. sql - 아래 list
-					
-					//4. 실행객체 & 데이터 세팅
-					pstmt = con.prepareStatement(WRITE);
-					pstmt.setString(1, ((BoardVO) vo).getTitle());
-					pstmt.setString(2, ((BoardVO) vo).getContent());
-					pstmt.setString(3, ((BoardVO) vo).getWriter());
-					pstmt.setString(4, ((BoardVO) vo).getPw());
-					
-					//5. 실행 실행 update : sxecuteUpdate() - int 결과가 나옴
-					result = pstmt.executeUpdate();
-					
-					//6. 표시 또는 담기
-					if(result == 0) {  //글번호가 존재하지 않는다 -> 예외로 처리한다
-						throw new Exception("글등록에 실패했습니다");
-					}//end of if
-					
-
-				}catch(Exception e ) {
-					e.printStackTrace();
-					//특별한 예외는 그냥 전달한다
-					if(e.getMessage().indexOf("예외발생") >= 0) throw e;
-					//그외 처리 중 나타나는 오류에 대해서 시용자가 볼수 있는 예외로 만들어 전달한다.
-					else throw new Exception("예외 발생: 게시판 글보기 조회수 DB처리 중 예외가 발생했습니다");
-				}finally {	
-					//7. 닫기
-					DB.close(con, pstmt);
-				}
-				
-				
-				//결과 데이터를 리턴해 준다.
-				return  null;
-			}// end of public int increase
+		//결과를 저장할 수 있는 변수 선언.
+		
+		int result = 0;
+		try {
+			//1. 드라이버 확인
+			
+			//2. 연결
+			con = DB.getConnection();
+			
+			//3. sql - 아래 list
+			
+			//4. 실행객체 & 데이터 세팅
+			pstmt = con.prepareStatement(WRITE);
+			pstmt.setString(1, ((BoardVO) vo).getTitle());
+			pstmt.setString(2, ((BoardVO) vo).getContent());
+			pstmt.setString(3, ((BoardVO) vo).getWriter());
+			pstmt.setString(4, ((BoardVO) vo).getPw());
+			
+			//5. 실행 실행 update : sxecuteUpdate() - int 결과가 나옴
+			result = pstmt.executeUpdate();
+			
+			//6. 표시 또는 담기
+			if(result == 0) {  //글번호가 존재하지 않는다 -> 예외로 처리한다
+				throw new Exception("글등록에 실패했습니다");
+			}//end of if
+			
+	
+		}catch(Exception e ) {
+			e.printStackTrace();
+			//특별한 예외는 그냥 전달한다
+			if(e.getMessage().indexOf("예외발생") >= 0) throw e;
+			//그외 처리 중 나타나는 오류에 대해서 시용자가 볼수 있는 예외로 만들어 전달한다.
+			else throw new Exception("예외 발생: 게시판 글보기 조회수 DB처리 중 예외가 발생했습니다");
+		}finally {	
+			//7. 닫기
+			DB.close(con, pstmt);
+		}
+		
+		
+		//결과 데이터를 리턴해 준다.
+		return  null;
+	}// end of public int increase
 		
 		
 			
@@ -203,71 +193,71 @@ public class BoardDAO extends DAO {
 	//1.리스트 처리
 	//BoardController -> (Execute로그 출력) -> BoardListService -> [BoardDAO.view()]
 	public int update (BoardVO vo)  throws Exception{
-					//결과를 저장할 수 있는 변수 선언
-					int result = 0;
-				
-					try {
-						//1. 드라이버 확인
-						//2. 연결
-						con = DB.getConnection();
-						//3. sql - 아래 list
-						//4. 실행객체 & 데이터 세팅
-						pstmt = con.prepareStatement(UPDATE);
-						pstmt.setString(1, vo.getTitle());
-						pstmt.setString(2, vo.getContent());
-						pstmt.setString(3, vo.getWriter());
-						pstmt.setLong(4, vo.getNo());
-						pstmt.setString(5, vo.getPw());
-						//5. 실행
-						result = pstmt.executeUpdate();
-						//6. 표시 또는 담기
-						if(result == 0) {	
-							throw new Exception("예외 발생 : 글번호나 비밀번호가 맞지 않습니다. 정보를 확인해 주세요");
-							}//end of if
-						}catch(Exception e ) {
-						  e.printStackTrace();
-						  throw e;
-					}finally {
-					}//7. 닫기
-						DB.close(con, pstmt, rs);	
-					//결과 데이터를 리턴해 준다.
-					return result;
-				}//end of public view
+		//결과를 저장할 수 있는 변수 선언
+		int result = 0;
+	
+		try {
+			//1. 드라이버 확인
+			//2. 연결
+			con = DB.getConnection();
+			//3. sql - 아래 list
+			//4. 실행객체 & 데이터 세팅
+			pstmt = con.prepareStatement(UPDATE);
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getContent());
+			pstmt.setString(3, vo.getWriter());
+			pstmt.setLong(4, vo.getNo());
+			pstmt.setString(5, vo.getPw());
+			//5. 실행
+			result = pstmt.executeUpdate();
+			//6. 표시 또는 담기
+			if(result == 0) {	
+				throw new Exception("예외 발생 : 글번호나 비밀번호가 맞지 않습니다. 정보를 확인해 주세요");
+				}//end of if
+			}catch(Exception e ) {
+			  e.printStackTrace();
+			  throw e;
+		}finally {
+		}//7. 닫기
+			DB.close(con, pstmt, rs);	
+		//결과 데이터를 리턴해 준다.
+		return result;
+	}//end of public view
 					
 				
 	//5. 삭제
 	//1.리스트 처리
 	//BoardController -> (Execute로그 출력) -> BoardListService -> [BoardDAO.view()]
 	public int delete (BoardVO vo)  throws Exception{
-						//결과를 저장할 수 있는 변수 선언
-						int result = 0;
-					
-						try {
-							//1. 드라이버 확인
-							//2. 연결
-							con = DB.getConnection();
-							//3. sql - 아래 list
-							//4. 실행객체 & 데이터 세팅
-							pstmt = con.prepareStatement(DELETE);
-							pstmt.setLong(1, vo.getNo());
-							pstmt.setString(2, vo.getPw());
-							//5. 실행
-							result = pstmt.executeUpdate();
-							//6. 표시 또는 담기
-							if(result == 0) {	//글번호가 존재하지 않거나 비번 틀림 -> 예외로 처리한다 
-								throw new Exception("예외 발생 : 글번호나 비밀번호가 맞지 않습니다. 정보를 확인해 주세요");
-								}//end of if
-							}catch(Exception e ) {
-							  e.printStackTrace();
-							  if(e.getMessage().indexOf("예외발생") >= 0 ) throw e;
-							  
-							  else throw new Exception("에외 발생 : 게시판 글삭제 DB 처리 중 예외가 발생했씁니다");
-						}finally {
-						}//7. 닫기
-							DB.close(con, pstmt);	
-						//결과 데이터를 리턴해 준다.
-						return result;
-					}//end of public view
+		//결과를 저장할 수 있는 변수 선언
+		int result = 0;
+	
+		try {
+			//1. 드라이버 확인
+			//2. 연결
+			con = DB.getConnection();
+			//3. sql - 아래 list
+			//4. 실행객체 & 데이터 세팅
+			pstmt = con.prepareStatement(DELETE);
+			pstmt.setLong(1, vo.getNo());
+			pstmt.setString(2, vo.getPw());
+			//5. 실행
+			result = pstmt.executeUpdate();
+			//6. 표시 또는 담기
+			if(result == 0) {	//글번호가 존재하지 않거나 비번 틀림 -> 예외로 처리한다 
+				throw new Exception("예외 발생 : 글번호나 비밀번호가 맞지 않습니다. 정보를 확인해 주세요");
+				}//end of if
+			}catch(Exception e ) {
+			  e.printStackTrace();
+			  if(e.getMessage().indexOf("예외발생") >= 0 ) throw e;
+			  
+			  else throw new Exception("에외 발생 : 게시판 글삭제 DB 처리 중 예외가 발생했씁니다");
+		}finally {
+		}//7. 닫기
+			DB.close(con, pstmt);	
+		//결과 데이터를 리턴해 준다.
+		return result;
+	}//end of public view
 								
 				
 
