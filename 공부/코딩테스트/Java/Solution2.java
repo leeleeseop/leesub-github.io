c5d8a949da6f770e2d6f406fe14209e6fe45d7ae
leeleeseop <iseobi138@gmail.com>
2025-03-27 22:45:10 +0900

    두 수의 곱 구하기

---
+++ 공부/코딩테스트/Java/Solution2.java
@@ -0,0 +1,15 @@
+// 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+// 입출력 예1: num1이 3, num2가 4이므로 3 * 4 = 12를 return합니다.
+// 입출력 예1: num1이 27, num2가 19이므로 27 * 19 = 513을 return합니다.
+
+class Solution {
+    // solution이라는 메서드를 선언  매개변수로 num1과 num2를 입력을 받음
+    public int solution(int num1, int num2) {
+        // num1과 num2 곱한 값을 answer에 저장
+        int answer = num1 * num2;
+        // 곱한 값을 반환
+        return answer;
+    }
+}
+
+// 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
