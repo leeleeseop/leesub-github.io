14c53fc74f86fd560671c169eaa31ee14ff01212
leeleeseop <iseobi138@gmail.com>
2025-03-27 22:36:59 +0900

    나눈 값의 몫 구하기

---
+++ 공부/코딩테스트/Java/Solution1.java
@@ -0,0 +1,16 @@
+// 정수 num1, num2가 매개변수로 주어질 때 num1을 num2로 나눈 몫을 return 하도록 solution함수를 완성해주세요.
+// 입출력 예1: num1이10, num2가 5이므로 10을5로 나눈 몫을 2를 리턴
+// 입출력 예2: num1이7, num2가 2이므로 7을2로 나눈 몫을 3를 리턴
+
+class Solution{
+
+  // solution이라는 메서드를 선언  매개변수로 num1과 num2를 입력을 받음
+  public int solution{int num1. int num2}{
+    // num1과 num2를 나눈 값을 answer에 저장
+    int answer = num1 / num2;
+    // 나눈 몫을 반환
+    return answer;
+  }
+}
+
+// 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
