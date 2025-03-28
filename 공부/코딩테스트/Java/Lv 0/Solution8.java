1370308b34b70285ee3d47dda30f60f5bde43c45
leeleeseop <iseobi138@gmail.com>
2025-03-28 17:55:13 +0900

    나머지 구하기

---
+++ 공부/코딩테스트/Java/Lv 0/Solution8.java
@@ -0,0 +1,14 @@
+// 문제설명: 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+// 입출력예1: num1이 3, num2가 2이므로 3을 2로 나눈 나머지 1을 return 합니다.
+// 입출력예2: num1이 10, num2가 5이므로 10을 5로 나눈 나머지 0을 return 합니다.
+
+class Solution{
+  // solution이라는 메서드를 선언  매개변수로 num1과 num2를 입력을 받음
+  public int solution(int num1, int num2){
+    // %는 나눗셈의 나머지를 산출하는 연산으로 나머지의 값은 answe에 저장
+    int answe = num1 % num2;
+    // 나머지의 값은 반환
+    retunr answer;
+  }
+}
+// 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
