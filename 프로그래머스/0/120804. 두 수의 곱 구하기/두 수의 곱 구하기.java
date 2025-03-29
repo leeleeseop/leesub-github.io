d79fe9a5b8612a82601d88cdd1d5e04005ff3633
leeleeseop <iseobi138@gmail.com>
2025-03-29 13:34:51 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.03 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    // solution이라는 메서드를 선언  매개변수로 num1과 num2를 입력을 받음
+    public int solution(int num1, int num2) {
+        // num1과 num2 곱한 값을 answer에 저장
+        int answer = num1 * num2;
+        // 곱한 값을 반환
+        return answer;
+    }
+}
