4db597a0f159189ba275b982a791091808aa8bbe
leeleeseop <iseobi138@gmail.com>
2025-03-29 13:33:49 +0900

    [level 0] Title: 나머지 구하기, Time: 0.03 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    // solution이라는 메서드를 선언  매개변수로 num1과 num2를 입력을 받음
+    public int solution(int num1, int num2) {
+        // %는 나눗셈의 나머지를 answe에 저장
+        int answer = num1 % num2;
+        // 나머지의 값은 반환
+        return answer;
+    }
+}
