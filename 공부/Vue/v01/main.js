ef9efc4bc8ed01969c62bf028898dc6ca5bcee5f
leeleeseop <iseobi138@gmail.com>
2025-05-24 17:52:27 +0900

    Delete 공부/Vue/v01 directory

--- 공부/Vue/v01/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-/* App.vue의 내용들을 담아서 (App)에 생성하고 mount('#app') id가 app인 영역에 붙인다*/
-createApp(App).mount('#app')
