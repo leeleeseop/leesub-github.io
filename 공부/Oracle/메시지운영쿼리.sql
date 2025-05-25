f8582e73645a3a2b18518133e5ae2a03951eabbf
leeleeseop <iseobi138@gmail.com>
2025-05-25 21:33:42 +0900

    Delete 공부/Oracle directory

--- 공부/Oracle/메시지운영쿼리.sql
+++
@@ -1,9 +0,0 @@
---- 메시지 운영 쿼리
--- 1. 리스트
-select
-    m.no, m.content, m.senderID, sm.name, m.sendDate, 
-    m.accepterID, am.name, m.acceptDate
-from message m, member sm, member am
-
--- 조건((일반조건) and (조인조건)) : 조건이 없으면 생략 가능
-where m.senderID = sm.id and accpterID = am.id;
